13342683770a9447ab189b0fc9fe5a01f12bd2b1
Cesar Ibarra <ibarratcesar@icloud.com>
2025-03-30 12:25:55 -0700

    Initial Commit

---
+++ GoogleAdMobTest/GoogleAdMobTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleAdMobTestApp.swift
+//  GoogleAdMobTest
+//
+//  Created by Cesar Ibarra on 3/30/25.
+//
+
+import SwiftUI
+
+@main
+struct GoogleAdMobTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
