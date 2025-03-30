8dc98c9a163e18cbb4ab9fda1192eebe10a3208f
Cesar Ibarra <ibarratcesar@icloud.com>
2025-03-30 13:56:49 -0700

    initial commit

---
+++ GoogleAdMobTest/InterstitialAd.swift
@@ -0,0 +1,8 @@
+//
+//  InterstitialAd.swift
+//  GoogleAdMobTest
+//
+//  Created by Cesar Ibarra on 3/30/25.
+//
+
+import Foundation
---
+++ GoogleAdMobTest/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  GoogleAdMobTest
+//
+//  Created by Cesar Ibarra on 3/30/25.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
